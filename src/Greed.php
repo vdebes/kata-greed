e8a67776753b206b3430eb4c4a20cc5b1bb98258
vdebes <vdebes.externe@bedrockstreaming.com>
2022-01-16 11:35:05 +0100

    score from triple 1

--- src/Greed.php
+++ src/Greed.php
@@ -17,9 +17,21 @@ class Greed
 
     public function getScore(): int
     {
-        $singleOnes = array_map(static fn (int $roll) => $roll === 1 ? 100 : 0, $this->rolls);
+        $occurences = array_count_values($this->rolls);
+        $tripleOnes = array_filter(
+            array_map(
+                static fn (int $value, int $key) => $key === 1 && $value === 3 ? 1000 : null,
+                $occurences,
+                array_keys($occurences)
+            )
+        );
+
+        $singleOnes = [];
+        if (empty($tripleOnes) === true) {
+            $singleOnes = array_map(static fn (int $roll) => $roll === 1 ? 100 : 0, $this->rolls);
+        }
         $singleFives = array_map(static fn (int $roll) => $roll === 5 ? 50 : 0, $this->rolls);
 
-        return array_sum(array_merge($singleOnes, $singleFives));
+        return array_sum(array_merge($singleOnes, $singleFives, $tripleOnes));
     }
 }

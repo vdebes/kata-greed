38e570c3b3c7cd70051a56aeeac060177e479f52
vdebes <vdebes.externe@bedrockstreaming.com>
2022-01-16 12:04:09 +0100

    refactor getTriples

--- src/Greed.php
+++ src/Greed.php
@@ -34,15 +34,15 @@ class Greed
     private function getTriples(): array
     {
         $occurences = array_count_values($this->rolls);
-        $tripleOnes = array_filter(
-            array_map(
-                static fn (int $value, int $key) => $key === 1 && $value === 3 ? 1000 : null,
-                $occurences,
-                array_keys($occurences)
-            )
-        );
+        $triples = [];
+        $search = [1];
+        foreach ($occurences as $value => $occurenceCount) {
+            if (in_array($value, $search) === true && $occurenceCount === 3) {
+                $triples[$value] = 1000;
+            }
+        }
 
-        return $tripleOnes;
+        return $triples;
     }
 
     /**

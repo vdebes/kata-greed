101b938899fc3b89854cdd337976d82dad68b7d5
vdebes <vdebes.externe@bedrockstreaming.com>
2022-01-16 10:18:53 +0100

    rolls as construct arguments

--- tests/GreedTest.php
+++ tests/GreedTest.php
@@ -11,4 +11,12 @@ class GreedTest extends TestCase
         self::assertIsObject($testedInstance);
         self::assertIsInt($testedInstance->getScore());
     }
+
+    public function test greed is constructed from dice rolls results(): void
+    {
+        $rolls = [1, 2, 4];
+        $testedInstance = new Greed($rolls);
+
+        self::assertEquals(array_sum($rolls), $testedInstance->getScore());
+    }
 }

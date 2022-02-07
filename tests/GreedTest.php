d081fe0d6b5c02a941e56859743047757be129f5
Christophe Moncy <cmoncy.externe@bedrockstreaming.com>
2022-02-07 10:21:54 +0100

    first iteration

--- tests/GreedTest.php
+++ tests/GreedTest.php
@@ -9,4 +9,12 @@ class GreedTest extends TestCase
     {
         self::assertIsObject(new Greed());
     }
+
+    public function testInput(): void
+    {
+        $someValue = 42;
+        $greed = new Greed();
+        $greed->score($someValue);
+        self::expectException(\Exception::class);
+    }
 }

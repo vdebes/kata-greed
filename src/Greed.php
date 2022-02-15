68b738c52961fabd5aa970f73303c732eb90aa98
Valentin Claras <valentin-claras@users.noreply.github.com>
2022-02-15 14:28:25 +0100

    Feat/initial test (#3)
    Add kickstart + clarify rules

--- src/Greed.php
+++ src/Greed.php
@@ -4,7 +4,11 @@ namespace Vdebes\KataGreed;
 
 class Greed
 {
-    public function score(): void
+    /**
+     * @param array{0: int, 1: int, 2: int, 3: int, 4: int, 5: int} $dice
+     */
+    public function score(array $dice): int
     {
+        return 0;
     }
 }
